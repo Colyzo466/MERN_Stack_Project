2cdebed0c042c9fb9fd1cfabfd8b7252409f9ceb
victor oguike <colyzo466@gmail.com>
2025-04-04 05:14:27 +0100

    Initial commit

---
+++ server/db/connection.js
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv"; // Correctly import dotenv
+
+// Load environment variables from .env file
+dotenv.config();
+const uri = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+
+});
+
+try {
+
+  // Connect the client to the server
+  await client.connect();
+
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+
+  console.log( "Pinged your deployment. You successfully connected to MongoDB!" );
+
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+export default db;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+import dotenv from "dotenv"; // Correctly import dotenv
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
